709fed00ebfa6f0a09a6ba3f22a4a41e3b63127a
John Michael Robey <robey14@gmail.com>
2020-01-26 21:21:44 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
 def using_until
   phrase = 6
-  until phrase = 10 
-  puts "Wingardium Leviosa"
-  phrase += 1
-  
+  until phrase == 10 
+    puts "Wingardium Leviosa"
+    phrase += 1
   end  
 end
 
